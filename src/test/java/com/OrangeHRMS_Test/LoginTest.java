45a16c3244d14a0c9efa92527b64e6cfd606f7b0
K Sudarshan Rao <sudarshan.kondam@quisitive.com>
2024-11-13 15:28:46 +0530

    Adding project

---
+++ src/test/java/com/OrangeHRMS_Test/LoginTest.java
@@ -0,0 +1,48 @@
+package com.OrangeHRMS_Test;
+
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+
+
+public class LoginTest {
+
+	WebDriver driver;
+	String URL = "https://opensource-demo.orangehrmlive.com/web/index.php/auth/login";
+	
+	@BeforeTest
+	public void Before() {
+	
+		System.setProperty("webdriver.chrome.driver", System.getProperty("user.dir") + "/Drivers/chromedriver.exe");
+		driver = new ChromeDriver();
+		System.out.println("The browser value is Chrome");
+		driver.get(URL);
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5000));
+		driver.manage().window().maximize();
+		}
+	@Test
+	public void Login() throws InterruptedException {
+		driver.findElement(By.name("username")).click();
+		driver.findElement(By.name("username")).clear();
+		driver.findElement(By.name("username")).sendKeys("Admin");
+		
+		driver.findElement(By.name("password")).click();
+		driver.findElement(By.name("password")).clear();
+		driver.findElement(By.name("password")).sendKeys("admin123");
+		Thread.sleep(5000);
+		//driver.findElement(By.xpath("@type,'submit'")).click();
+		
+	}
+	@AfterTest
+	public void quit() {
+		driver.quit();
+	}
+	
+
+}
